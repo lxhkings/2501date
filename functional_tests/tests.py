a5bcc62a4b42dfd456e333ca10d6dcf44b50e750
xiaohong <lxhkings@gmail.com>
2018-04-23 20:41:07 +0800

    FT布局+样式的第一步

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -134,3 +134,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+        # 测试布局和样式
+    def test_layout_and_styling(self):
+        # 伊迪丝的主页
+        self.browser.get(self.live_server_url)
+        # 窗口大小变化
+        self.browser.set_window_size(1024, 768)
+
+        # 她注意到输入框为中心
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # 通过让我们指定我们希望我们的算术在正负10个像素范围内工作，可以帮助我们
+        # 处理四舍五入错误以及由于滚动条等引起的偶然奇怪现象。
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # 她开始一个新的列表，看到输入是好的
+        # 也在中间
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
