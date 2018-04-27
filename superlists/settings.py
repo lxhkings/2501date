22b2e1f8dabed86dcdbf103d1e0cdc6b72f748fb
xiaohong <lxhkings@gmail.com>
2018-04-27 09:04:21 +0800

    使用environ进行产品设置DEBUG,ALLOWED_HOST,SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,13 +19,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c8u-ipn1uz#u90^7btb3+zj03ypo_-7(91^!2hupd3-c!ls5-^'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'c8u-ipn1uz#u90^7btb3+zj03ypo_-7(91^!2hupd3-c!ls5-^'
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+#
+# ALLOWED_HOSTS = ['*']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'c8u-ipn1uz#u90^7btb3+zj03ypo_-7(91^!2hupd3-c!ls5-^'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
